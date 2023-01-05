466b00eca0e656f5c5f5076d20242ae6304dba8c
thin-paribaker <pari@thinknimble.com>
2023-01-04 22:24:46 -0800

    msg

--- s3-upload.py
+++ s3-upload.py
@@ -2,8 +2,8 @@ import os
 import boto3
 from botocore.exceptions import NoCredentialsError
 
-ACCESS_KEY = os.environ.get('AWS_KEY_ID')
-SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+ACCESS_KEY = os.environ.get('ACCESS_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY)
 print(ACCESS_KEY)
 
 
