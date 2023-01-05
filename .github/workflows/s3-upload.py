42430f5cd7aedf4fc32d2b880fdb8841e7085a8b
thin-paribaker <pari@thinknimble.com>
2023-01-04 22:13:37 -0800

    msg

---
+++ .github/workflows/s3-upload.py
@@ -0,0 +1,25 @@
+import os
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+ACCESS_KEY = os.environ.get('AWS_KEY_ID')
+SECRET_KEY = os.environ.get('AWS_KEY_ID')
+
+
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+
+uploaded = upload_to_aws('s3-upload.py', 'tn-s3-cloud-formation', 'tst.sh')
